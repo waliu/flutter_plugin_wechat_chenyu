468cee06ce263398a83b8975bb1ff2d7c50e9c94
waliu <250187715@qq.com>
2022-01-05 08:43:24 +0800

    Update Pay.java
    优化写法

--- android/src/main/java/com/jxm/flutter_plugin_wechat_chenyu/Pay.java
+++ android/src/main/java/com/jxm/flutter_plugin_wechat_chenyu/Pay.java
@@ -17,18 +17,14 @@ public class Pay {
 
   public void sendPaymentRequest(JSONObject json,String appId) throws RuntimeException {
     PayReq req = new PayReq();
-    try {
-      req.appId = appId;  // appId
-      req.partnerId = json.getString("partnerId");
-      req.prepayId = json.getString("prepayId");
-      req.nonceStr = json.getString("nonceStr");
-      req.timeStamp = json.getString("timestamp");
-      req.packageValue = json.getString("packageValue");
-      req.sign = json.getString("sign");
-      req.extData = "app data"; // optional
-    } catch (JSONException e) {
-      throw new RuntimeException(Constants.Error501);
-    }
+    req.appId = appId;  // appId
+    req.partnerId = json.getString("partnerId");
+    req.prepayId = json.getString("prepayId");
+    req.nonceStr = json.getString("nonceStr");
+    req.timeStamp = json.getString("timestamp");
+    req.packageValue = json.getString("packageValue");
+    req.sign = json.getString("sign");
+    req.extData = "app data"; // optional
     boolean sendRes = api.sendReq(req);
     if (!sendRes) {
       throw new RuntimeException(Constants.Error503);
